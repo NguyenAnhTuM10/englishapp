1e4bc4cb0b0f4cd1b4523c89371597c934a3a601
NguyenAnhTuM10 <151209084+NguyenAnhTuM10@users.noreply.github.com>
2026-04-28 15:52:05 +0700

    feat: init react frontend with vite + typescript + tailwind + shadcn

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
